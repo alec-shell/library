8926f55684ec909da136a260547b14c9c2b35f8e
alec-shell <143364125+alec-shell@users.noreply.github.com>
2025-02-25 23:22:45 -0500

    added basic page layout and styling. Hard-coded some JS funtionality for initial testing.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-console.log("Odin-Library");
+let myLibrary = [];
 
-myLibrary = [];
+myLibrary[0] = new Book("The Good Earth", "Pearl Buckman", 302);
+myLibrary[1] = new Book("1984", "George Orwell", 324);
+myLibrary[2] = new Book("The Pillars of the Earth", "Ken Follett", 1040);
+myLibrary[3] = new Book("Brave New Word", "Aldous Huxley", 250);
+
+
+let container = document.querySelector(".content-container");
+let newEntry = document.querySelector(".create-book");
+
+myLibrary.forEach((book) => displayBook(book));
+
+newEntry.addEventListener("click", () => createBook());
 
 function Book(title, author, pages) {
     this.title = title;
@@ -8,10 +19,17 @@ function Book(title, author, pages) {
     this.pages = pages;
 };
 
+//create new Book instance
 function addBookToLibrary(title, author, pages) {
     newBook = new Book(title, author, pages);
-
-    myLibrary[`${title}`] = newBook;
+    myLibrary.append(newBook);
+    displayBook(newBook);
 };
 
-
+function displayBook(book) {
+    bookDisplay = document.createElement("div");
+    bookDisplay.innerHTML = `<h2>${book.title}</h2>
+                            <h3>Author: ${book.author}</h3>
+                            <h3>Pages: ${book.pages}</h3>`;
+    container.appendChild(bookDisplay);
+};
